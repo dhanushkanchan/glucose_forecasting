8533b73c721481daf0288996ec9b9a69034da69b
Dhanush Kanchan <dhanushkanchan@gmail.com>
2019-12-01 15:05:01 -0500

    refactored code

--- core/preprocessing.py
+++ core/preprocessing.py
@@ -6,24 +6,35 @@ from datetime import datetime
 
 from .config import cfg
 
-FMT = '%Y-%m-%d %H:%M:%S'
+FMT = "%Y-%m-%d %H:%M:%S"
 
-class Dataset():
+
+class Dataset:
     def __init__(self, dataframe):
         self.dataframe = dataframe
         self.params = cfg.DATA
 
-    def clean_df(self): 
-        self.data = self.dataframe.fillna(0).drop(columns = ['heart rate']).sort_values(by=['Time'])[self.dataframe.apply(lambda x: datetime.strptime(x['Time'],FMT) > datetime.strptime("2019-02-26", "%Y-%m-%d"), axis=1)]
+    def clean_df(self):
+        self.data = (
+            self.dataframe.fillna(0)
+            .drop(columns=["heart rate"])
+            .sort_values(by=["Time"])[
+                self.dataframe.apply(
+                    lambda x: datetime.strptime(x["Time"], FMT)
+                    > datetime.strptime("2019-02-26", "%Y-%m-%d"),
+                    axis=1,
+                )
+            ]
+        )
         self.values = self.data.values
         print(self.values.shape)
         return self
 
-    def clean_df_2(self, columns_list=['timestamp', 'glucose']):
-        self.data = self.dataframe.fillna(0)[columns_list].sort_values(by=['timestamp'])
+    def clean_df_2(self, columns_list=["timestamp", "glucose"]):
+        self.data = self.dataframe.fillna(0)[columns_list].sort_values(by=["timestamp"])
         self.values = self.data.values
         print(self.values.shape)
-        if len(columns_list)-1 != self.params.NUM_FEATURES :
+        if len(columns_list) - 1 != self.params.NUM_FEATURES:
             print("WARNING!!! Number of features do not match number of columns given!")
         return self
 
@@ -31,13 +42,13 @@ class Dataset():
         k1 = self.params.INPUT_TIMESTEPS
         k2 = self.params.OUTPUT_TIMESTEPS
         n = len(self.values)
-        
+
         self.X = []
         self.Y = []
 
-        for i in range(n-k2-k1):
-            self.X.append(self.values[i:i+k1, y_index:])
-            self.Y.append(self.values[i+k1:i+k1+k2, y_index])
+        for i in range(n - k2 - k1):
+            self.X.append(self.values[i : i + k1, y_index:])
+            self.Y.append(self.values[i + k1 : i + k1 + k2, y_index])
 
         self.X = np.array(self.X)
         self.Y = np.array(self.Y)
@@ -46,16 +57,16 @@ class Dataset():
 
     def series_to_supervised(self, y_index=1):
         self.__rolling_window(y_index)
-        split = int(self.params.VALIDATION_SPLIT*len(self.X))
+        split = int(self.params.VALIDATION_SPLIT * len(self.X))
 
-        if split%cfg.TRAIN.BATCH_SIZE != 0:
-            split = split - (split%cfg.TRAIN.BATCH_SIZE)
+        if split % cfg.TRAIN.BATCH_SIZE != 0:
+            split = split - (split % cfg.TRAIN.BATCH_SIZE)
         split2 = len(self.X) - split
-        
-        if split2%cfg.TRAIN.BATCH_SIZE != 0:
-            split2 = split2 - (split2%cfg.TRAIN.BATCH_SIZE)
+
+        if split2 % cfg.TRAIN.BATCH_SIZE != 0:
+            split2 = split2 - (split2 % cfg.TRAIN.BATCH_SIZE)
         x_train, y_train = self.X[:split], self.Y[:split]
-        x_val, y_val = self.X[split:split+split2], self.Y[split:split+split2]
+        x_val, y_val = self.X[split : split + split2], self.Y[split : split + split2]
 
         return x_train, y_train, x_val, y_val
 
@@ -66,6 +77,6 @@ class Dataset():
         for group in groups:
             plt.subplot(len(groups), 1, i)
             plt.plot(self.values[:, group])
-            plt.title(self.data.columns[group], y=0.5, loc='right')
+            plt.title(self.data.columns[group], y=0.5, loc="right")
             i += 1
         plt.show()
